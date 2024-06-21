49e7ea9df94bb385e305ba6c913f868252fcbc91
AKharitonashvili <sandroxari@gmail.com>
2024-06-21 21:49:00 +0400

    feature layout

---
+++ client/sales-manager-application/src/app/shared/ui/layouts/page-layout/page-layout.component.ts
@@ -0,0 +1,37 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ButtonComponent } from '../../buttons/button/button.component';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { ToFormGroup } from 'src/app/models/shared.moelds';
+import { MatInputModule } from '@angular/material/input';
+
+@Component({
+  selector: 'app-page-layout',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    ButtonComponent,
+    ReactiveFormsModule,
+    MatPaginatorModule,
+    MatInputModule,
+  ],
+  templateUrl: './page-layout.component.html',
+  styleUrls: ['./page-layout.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PageLayoutComponent {
+  @Input() filterForm!: FormGroup<
+    ToFormGroup<{ filterControl: string | null }>
+  >;
+
+  @Output() openAddDialog = new EventEmitter();
+}
